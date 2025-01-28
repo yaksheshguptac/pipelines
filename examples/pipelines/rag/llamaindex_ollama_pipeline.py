00b258a504d2dd4a34c6294b696fef237abb8e72
yaksheshguptac <145982920+yaksheshguptac@users.noreply.github.com>
2025-01-29 01:09:19 +0530

    Update llamaindex_ollama_pipeline.py

--- examples/pipelines/rag/llamaindex_ollama_pipeline.py
+++ examples/pipelines/rag/llamaindex_ollama_pipeline.py
@@ -51,7 +51,7 @@ class Pipeline:
         # This function is called when the server is started.
         global documents, index
 
-        data_dir = "/app/backend/data"
+        data_dir = "/home/azureuser/data"
         files = os.listdir(data_dir)
 
         print(f"Contents of {data_dir}:")
